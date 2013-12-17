be5532a79ccb60ece6c22cb7ffa55f0dc183dffd
Eric Durán <ericddm05@gmail.com>
2013-12-17 16:47:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Racqboard::Application.config.secret_token = 'f303133fa97db27a8c387017a0e0c63d93d3d34b78e2933c037458cb993dd36eb2201646a88e8a749f74c95d31a99d43e923ad11ec319a2fed2e812beaf23f24'
